a19f3814b398e2955e07956e981b4e39d4708580
Mahmudumar <mahmudumar476@gmail.com>
2024-10-31 23:11:55 +0100

    with effort to automate everything

--- app_compile.py
+++ app_compile.py
@@ -5,18 +5,29 @@ app_name = 'BobsiMo Minute Recorder'
 
 
 
-#do the compilation
+def make_app():#do the compilation
 
-yo=f"""\
-pyinstaller --onefile --noconsole \
---hidden-import=pyaudio --hidden-import=speech_recognition \
--n "{app_name}" -i "./BMT_logo.ico" test2.py
-"""
-print(yo)
-os.system(yo)
+    yo=f"""\
+    pyinstaller --onefile --noconsole \
+    --hidden-import=pyaudio --hidden-import=speech_recognition \
+    -n "{app_name}" -i "./BMT_logo.ico" test2.py
+    """
+    print(yo)
+    os.system(yo)
 
 
-#copy image into the app dist
-shutil.copy(r'.\\BMT_logo.ico', '.\\dist\\')
+    #copy image into the app dist
+    shutil.copy(r'.\\BMT_logo.ico', '.\\dist\\')
 
-print('done')
+    print('done')
+
+
+def app_installer_maker():
+    # os.makedirs(f'./FINAL/{app_name}')
+    #make an installer
+    shutil.move('./dist/BobsiMo Minute Recorder.exe',f'./FINAL/{app_name}/BobsiMo Minute Recorder.exe')
+    shutil.copy('./dist/BMT_logo.ico',f'./FINAL/{app_name}/BMT_logo.ico')
+    shutil.copy(f'./FINAL/{app_name}/BMT_logo.ico', './FINAL')
+
+# make_app()
+app_installer_maker()
